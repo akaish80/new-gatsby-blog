57c13c0f2137e41fd42d3abf08d6d5aa5ea2cef9
ArunKumar <ArunKumar.D@cognizant.com>
2021-05-11 15:15:19 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,75 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+import Styled from "styled-components"
+
+const BlogLink = Styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = Styled('h3')`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Seo title="Home" />
+    <div>
+      <h1>Arun's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitle>
+          </BlogLink>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+    {/* <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p> */}
+  </Layout>
+)
+
+export default IndexPage
+
+export const query = graphql`
+  {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
